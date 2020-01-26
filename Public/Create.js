6b2f42e4cdb4f60b6ffbc4d627f0831530fbd437
tjballworm <44422907+tjballworm@users.noreply.github.com>
2020-01-26 10:52:08 -0600

    create page DONE

--- Public/Create.js
+++ Public/Create.js
@@ -12,3 +12,43 @@ function displayChange(){
         x.style.display = "none"
     }
 }
+
+/*Information Validation*/
+document.getElementById("submitBtn").onclick = function() {infoError()};
+
+
+function infoError(){
+
+    /*Variables to make it simpler*/
+    var eventName = document.getElementById("eventName").value;
+    var eventCreator = document.getElementById("eventCreator").value;
+    var typeOfGame = document.getElementById("typeOfGame").value ;
+    var email = document.getElementById("email").value;
+    var phoneNumber = document.getElementById("phoneNumber").value;
+    var location  = document.getElementById("locationSpec").value;
+    var startDate = document.getElementById("startDate").value;
+    var endDate = document.getElementById("endDate").value;
+
+    var var_names = [eventName, eventCreator, typeOfGame, email, Number.parseInt(phoneNumber), location, startDate, endDate];
+    var i;
+    
+    console.log(var_names)
+    /*Empty Box checker*/
+    for (i = 0; i < var_names.length; i++) {
+        if (var_names[i]){
+        }else{
+            alert("You left some field empty!")
+        }
+    }
+
+    if (isNaN(phoneNumber)) {
+        alert("Your phone number is invalid!")
+    }
+
+
+    
+
+
+
+
+}
