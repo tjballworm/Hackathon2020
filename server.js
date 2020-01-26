5eff81ba342c7a9fa468d21093fd9baf1658fdb9
Zach Strombeck <zstrombeck@bis.midco.net>
2020-01-25 20:55:47 -0600

    changed dependeicies

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require('path');
 
 
 const app = express();
@@ -10,11 +11,11 @@ var ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
 app.use(express.static('Public'));
 
 app.get("/", (req, res)=>{
-    res.sendFile("./Public/Home.html");
+    res.sendFile(path.resolve("./Public/Home.html"));
 });
 
 app.get("/Home", (req, res)=>{
-    res.sendFile("./Public/Home.html");
+    res.sendFile(path.resolve("./Public/Home.html"));
 });
 
 
@@ -22,42 +23,42 @@ app.get("/Home", (req, res)=>{
 
 
 
-// POSTS
-app.post('/register', async (req, res) =>{
+// // POSTS
+// app.post('/register', async (req, res) =>{
 
-    //validate data before making a user
-    const {error} = registerValidation(req.body);
-    if(error){
-        return res.status(400).send(error.details[0].message);
-    } 
+//     //validate data before making a user
+//     const {error} = registerValidation(req.body);
+//     if(error){
+//         return res.status(400).send(error.details[0].message);
+//     } 
 
-    //Verify email is not already in the system
-    const emailAlreadyExists = await User.findOne({email: req.body.email});
+//     //Verify email is not already in the system
+//     const emailAlreadyExists = await User.findOne({email: req.body.email});
 
-    if(emailAlreadyExists){
-        return res.status(400).send("Email already exists");
-    }
+//     if(emailAlreadyExists){
+//         return res.status(400).send("Email already exists");
+//     }
 
-    //Hash the password
-    const salt = await bcrypt.genSalt(10);
-    const hashedPassword = await bcrypt.hash(req.body.password, salt);
-    console.log(User.listIndexes);
-    console.log('Got a post request');
-    const user = new User({
-        firstName: req.body.firstName,
-        lastName: req.body.lastName,
-        username: req.body.username,
-        email: req.body.email,
-        password: hashedPassword,
-    });
-    // /console.log(user);
-    try{
-        const savedUser = await user.save();
-        res.send({user: user.id});
-    } catch(err){
-        res.status(400).send(err);
-    }
-});
+//     //Hash the password
+//     const salt = await bcrypt.genSalt(10);
+//     const hashedPassword = await bcrypt.hash(req.body.password, salt);
+//     console.log(User.listIndexes);
+//     console.log('Got a post request');
+//     const user = new User({
+//         firstName: req.body.firstName,
+//         lastName: req.body.lastName,
+//         username: req.body.username,
+//         email: req.body.email,
+//         password: hashedPassword,
+//     });
+//     // /console.log(user);
+//     try{
+//         const savedUser = await user.save();
+//         res.send({user: user.id});
+//     } catch(err){
+//         res.status(400).send(err);
+//     }
+// });
 
 
 
