7ccfc647de3e49ac164abfc9980acc503640b344
Zach Strombeck <zstrombeck@bis.midco.net>
2020-01-25 18:52:24 -0600

    server is working!

--- server.js
+++ server.js
@@ -4,3 +4,21 @@ const app = express();
 
 var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080;
 var ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
+
+
+
+
+
+app.get("/", (req, res)=>{
+    res.send("You made it to the server, it's working");
+})
+
+
+
+
+// http://localhost:8080/
+
+
+app.listen(port, ip);
+console.log(`Server is Running! on IP:${ip} PORT:${port}`);
+module.expres = app;
