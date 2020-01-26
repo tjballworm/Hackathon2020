7812d489f0df29329f63c414a2e966700d0fb644
Zach Strombeck <zstrombeck@bis.midco.net>
2020-01-25 20:29:45 -0600

    added page directories

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080;
 var ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
 
 
-
+app.use(express.static('Public'));
 
 
 app.get("/", (req, res)=>{
