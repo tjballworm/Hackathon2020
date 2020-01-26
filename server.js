6ee423ea2d45a58720cb2c8ab154f150b4bd8a40
Kobe Ogoshi <43730605+ogoshifam@users.noreply.github.com>
2020-01-26 11:52:27 -0600

    fixng stuff

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
-const Event = require('./Event.js');
+const Event = require('./event.js');
 
 
 const app = express();
