b35291ab72834bede5f5237c50c06cc66c79548e
Zach Strombeck <zstrombeck@bis.midco.net>
2020-01-25 18:50:10 -0600

    added some modules we will need and a gitignore file

---
+++ server.js
@@ -0,0 +1,6 @@
+const express = require('express');
+
+const app = express();
+
+var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080;
+var ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
