4e7a38f3489704117354fbb927cb3804a192ebf3
tjballworm <44422907+tjballworm@users.noreply.github.com>
2020-01-26 02:02:12 -0600

    added dropdown change thing

--- Public/Create.js
+++ Public/Create.js
@@ -1,7 +1,14 @@
 
 
+/*Event Handler for Dropdown Change*/
+document.getElementById("locationDropdown").onchange = function() {displayChange()};
 
-function displayLoc(){
-    document.getElementById("locationSpec").style.display = "initial";
-    console.log("its working !")
+function displayChange(){
+    var x = document.getElementById("locationSpec");
+    var y = document.getElementById("locationDropdown");
+    if (y.selectedIndex == 1){
+        x.style.display = "initial"
+    }else{
+        x.style.display = "none"
+    }
 }
