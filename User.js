6f56305d8f9fdcf2c97a382a9cda3f65c4536d29
Zach Strombeck <zstrombeck@bis.midco.net>
2020-01-25 20:09:48 -0600

    added model

---
+++ User.js
@@ -0,0 +1,43 @@
+//todo copy this file to create an 'Event' model
+
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+    firstName:{
+        type: String,
+        required: true,
+        min : 1,
+        max : 255
+    },
+    lastName:{
+        type: String,
+        required: true,
+        min : 1,
+        max : 255
+    },
+    username:{
+        type: String,
+        required: true,
+        min : 1,
+        max : 255
+    },
+    email:{
+        type: String,
+        required: true,
+        min: 6,
+        max: 255
+    },
+    password: {
+        type: String,
+        required: true,
+        max: 1024,
+        min: 6
+    },
+    date: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+//Determines the collection it will be saved in users
+module.exports = mongoose.model('User', userSchema);
